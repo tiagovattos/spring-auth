a9d83d3d6ea8b4189d2e5f53976017b0fe4633b1
tiagovattos <tiagovattosz@gmail.com>
2024-08-28 20:08:04 -0300

    created user repository

---
+++ src/main/java/br/edu/fema/auth/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package br.edu.fema.auth.repository;
+
+import br.edu.fema.auth.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    UserDetails findByUsername(String username);
+}
