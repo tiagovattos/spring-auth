496452e73fe6866311f5dc33cf44fe2e765b69ae
tiagovattos <tiagovattosz@gmail.com>
2024-08-28 20:18:45 -0300

    created authentication service for loading user by username

---
+++ src/main/java/br/edu/fema/auth/service/AuthenticationService.java
@@ -0,0 +1,21 @@
+package br.edu.fema.auth.service;
+
+import br.edu.fema.auth.repository.UserRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthenticationService  implements UserDetailsService {
+    private final UserRepository userRepository;
+
+    public AuthenticationService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username);
+    }
+}
